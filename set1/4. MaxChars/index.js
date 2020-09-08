70d755ad0e3397ef354a01c5761202eea4e2b81e
Ilja Semjonov <ilsemjonov@gmail.com>
2020-09-08 16:02:41 +0300

    sol

--- set1/4. MaxChars/index.js
+++ set1/4. MaxChars/index.js
@@ -11,11 +11,7 @@ function maxChar(str) {
     let maxChar = '';
 
     for (let char of str) {
-        if (charMap[char]) {
-            charMap[char]++;
-        } else {
-            charMap[char] = 1;
-        }
+        charMap[char] = charMap[char] + 1 || 1;
     }
 
     for (let char in charMap) {
@@ -29,3 +25,26 @@ function maxChar(str) {
 }
 
 module.exports = maxChar;
+
+// function maxChar(str) {
+//     const charMap = {};
+//     let max = 0;
+//     let maxChar = '';
+
+//     for (let char of str) {
+//         if (charMap[char]) {
+//             charMap[char]++;
+//         } else {
+//             charMap[char] = 1;
+//         }
+//     }
+
+//     for (let char in charMap) {
+//         if (charMap[char] > max) {
+//             max = charMap[char];
+//             maxChar = char;
+//         }
+//     }
+
+//     return maxChar;
+// }
